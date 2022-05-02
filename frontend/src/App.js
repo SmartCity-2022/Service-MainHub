58f3c75e04126f8d99f07d3d0d0de9f5615a322a
pmayer1 <patrick_thomas.mayer@fh-bielefeld.de>
2022-05-02 17:07:20 +0200

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import {useState} from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
