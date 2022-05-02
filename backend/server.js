f0ac2f901b43f7b3928a531eb9d4b42b7920e487
Finn Amini Kaveh <finnamini2@gmail.com>
2022-05-02 15:44:56 +0200

    add jwt prototype

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+app.listen(3000)
+
+const authToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if(token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+
+const posts = [
+    {
+        username: 'Justin',
+        title: 'Richtig toller Post'
+    }, {
+        username: 'Paddy',
+        title: 'I ♥ react'
+    }, {
+        username: 'Finn',
+        title: 'Noch ein toller Post'
+    }
+]
+
+app.get('/posts', authToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
+})
