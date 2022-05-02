d36b8d23a44242de087ecb443f5cd49bfa572b73
pmayer1 <patrick_thomas.mayer@fh-bielefeld.de>
2022-05-02 18:47:19 +0200

    add database create/drop scripts

---
+++ database/create.sql
@@ -0,0 +1,26 @@
+CREATE TABLE Buerger(
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(64),
+    vorname varchar(64),
+    email varchar(64),
+    passwort varchar(64),
+    stadt varchar(64),
+    postleitzahl int(5),
+    strasse varchar(128),
+    telefon varchar(64),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Log(
+    id int NOT NULL AUTO_INCREMENT,
+    buerger_id int NOT NULL,
+    loginZeit timestamp DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (buerger_id) REFERENCES Buerger(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE AccessToken(
+    id int NOT NULL AUTO_INCREMENT,
+    Token varchar(255),
+    PRIMARY KEY (id)
+);
