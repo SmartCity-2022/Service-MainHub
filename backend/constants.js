3b1a51d4ac24153bc460804e5716bfe0d59a29c0
pmayer1 <patrick_thomas.mayer@fh-bielefeld.de>
2022-06-14 20:03:33 +0200

    add backend-sided validation

--- backend/constants.js
+++ backend/constants.js
@@ -8,6 +8,10 @@ module.exports = Object.freeze({
     ERR_INVALID_PASSWORD : 'Ungültiges Passwort.',
     ERR_INVALID_EMAIL : 'Ungültige Email.',
     ERR_INVALID_EMAIL_PASSWORD : 'Ungültige Email oder Passwort.',
+    ERR_INVALID_EMAIL_VALIDATION_FAILED : 'Bitte geben Sie eine gültige Email ein.',
+    ERR_INVALID_PASSWORD_MIN_LENGTH : 'Das Passwort muss mindestens 8 Zeichen lang sein.', 
+    ERR_INVALID_PASSWORD_MAX_LENGTH : 'Das Passwort darf maximal 30 Zeichen lang sein.',
+    ERR_INVALID_PASSWORD_MATCH : 'Die Passwörter stimmen nicht überein.',
 });
 
 // export const ERR_DUPLICATE_ENTRY = 'Duplicate entry';
